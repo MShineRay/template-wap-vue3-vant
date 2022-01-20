285d8114c5c8bc98a5306dcb75f1598800e4601a
msr <mshineray@qq.com>
2022-01-20 16:20:36 +0800

    upd: ++

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    styleImport({
+      resolves: [VantResolve()],
+    })
+  ],
+  build:{
+    // cssCodeSplit: false // false整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+    rollupOptions: { // 多页面
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
+  }
+})
